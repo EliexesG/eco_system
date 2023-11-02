753d982d04d4707c0b3781524fbe135f4b1245b6
EliexesG <107147734+EliexesG@users.noreply.github.com>
2023-11-02 13:23:35 -0600

    Mejora Cupones Index dividido por Categoria

--- client/src/app/cupon/cupon-index/cupon-index.component.ts
+++ client/src/app/cupon/cupon-index/cupon-index.component.ts
@@ -7,40 +7,72 @@ import { CuponDiagComponent } from '../cupon-diag/cupon-diag.component';
 @Component({
   selector: 'app-cupon-index',
   templateUrl: './cupon-index.component.html',
-  styleUrls: ['./cupon-index.component.css']
+  styleUrls: ['./cupon-index.component.css'],
 })
 export class CuponIndexComponent {
-  datos:any; //Respuesta del API
-  destroy$: Subject<boolean>= new Subject<boolean>();
+  datos: any; //Respuesta del API
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
-  constructor(private gService: GenericService, private dialog:MatDialog){
+  constructor(private gService: GenericService, private dialog: MatDialog) {
     this.listarCentrosAcopio();
   }
 
-  listarCentrosAcopio(){
+  listarCentrosAcopio() {
     //Solicitud al API para listar todos los centro de acopio
     //localhost:3000/centrosacopio
-    this.gService.list('cupon/')
+    this.gService
+      .list('cupon/validos')
       .pipe(takeUntil(this.destroy$))
-      .subscribe((response:any)=>{
+      .subscribe((response: any) => {
+        
         console.log(response);
-        this.datos=response;
-      })
+
+        let data: Array<{ categoria: string; cupones: any }> = [];
+        let usedCategorias: Array<string> = [];
+
+        response.forEach((cupon: any) => {
+          if (!usedCategorias.includes(cupon.categoria)) {
+            usedCategorias.push(cupon.categoria);
+            let cupones = response.filter(
+              (element: any) => element.categoria === cupon.categoria
+            );
+            data.push({
+              categoria: cupon.categoria.toLowerCase(),
+              cupones: cupones,
+            });
+          }
+        });
+
+        data = data.sort((a, b) => {
+          const categoriaA = a.categoria.toUpperCase();
+          const categoriaB = b.categoria.toUpperCase();
+          if (categoriaA < categoriaB) {
+            return -1;
+          }
+          if (categoriaA > categoriaB) {
+            return 1;
+          }
+          return 0;
+        });
+
+        console.log(data);
+
+        this.datos = data;
+      });
   }
 
-  
-  detalleCupon(id:number){
+  detalleCupon(id: number) {
     //Detalle en formato diálogo
-    const dialogConfig=new MatDialogConfig();
-    dialogConfig.disableClose=false;
-    dialogConfig.data={
-      id:id
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = false;
+    dialogConfig.data = {
+      id: id,
     };
-    this.dialog.open(CuponDiagComponent,dialogConfig);
+    this.dialog.open(CuponDiagComponent, dialogConfig);
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.destroy$.next(true);
     this.destroy$.unsubscribe();
-  } 
+  }
 }
