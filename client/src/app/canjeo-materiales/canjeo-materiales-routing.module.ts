456de7cc2f8b393dd1de1a5c723cabe03c943872
EliexesG <107147734+EliexesG@users.noreply.github.com>
2023-11-03 19:30:09 -0600

    Finalización detalle, all, index de Canjeo materi

--- client/src/app/canjeo-materiales/canjeo-materiales-routing.module.ts
+++ client/src/app/canjeo-materiales/canjeo-materiales-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CanjeoMaterialesIndexComponent } from './canjeo-materiales-index/canjeo-materiales-index.component';
+import { CanjeoMaterialesDetalleComponent } from './canjeo-materiales-detalle/canjeo-materiales-detalle.component';
+import { CanjeoMaterialesAllComponent } from './canjeo-materiales-all/canjeo-materiales-all.component';
 
 const routes: Routes = [
   {
     path: 'canjeomateriales',
     component: CanjeoMaterialesIndexComponent,
+  },
+  {
+    path: 'canjeomateriales/all',
+    component: CanjeoMaterialesAllComponent,
+  },
+  {
+    path: 'canjeomateriales/:id',
+    component: CanjeoMaterialesDetalleComponent,
   },
 ];
 
