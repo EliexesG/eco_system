d237b82ffbb115f41dedfe39a0fa40070374a063
Luis1999XD <luischg1999@outlook.es>
2023-11-14 22:46:56 -0600

    Sukitrukis

---
+++ client/src/app/cupon/cupon-all/cupon-all.component.ts
@@ -0,0 +1,77 @@
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { Subject, takeUntil } from 'rxjs';
+
+import { ActivatedRoute, Router } from '@angular/router';
+import { GenericService } from 'src/app/share/services/generic.service';
+
+@Component({
+  selector: 'app-cupon-all',
+  templateUrl: './cupon-all.component.html',
+  styleUrls: ['./cupon-all.component.css']
+})
+export class CuponAllComponent implements AfterViewInit {
+  datos: any;
+  destroy$: Subject<boolean> = new Subject<boolean>();
+
+  
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  dataSource: MatTableDataSource<any> = new MatTableDataSource<any>();
+  columns: string[] = [
+    'nombre',
+    'monedasUnidad',
+    'categoria',
+    'fechaInicio',
+    'fechaFin',
+    'acciones',
+  ];
+
+
+  constructor(private gService: GenericService, 
+    private router: Router,
+    private route: ActivatedRoute)
+  {
+
+  }
+
+  ngAfterViewInit(): void {
+    this.listarCupones();   
+  }
+  listarCupones(){
+    this.gService.list('cupon/')
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((response:any)=>{
+        console.log(response);
+        this.datos=response;
+        this.dataSource = new MatTableDataSource(this.datos);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+        
+      })
+  }
+  detalleCupon(id: number){
+    this.router.navigate(['/cupon',id],
+      {
+        relativeTo:this.route
+      }
+    )
+  }
+  actualizarCupon(id: number) {
+    this.router.navigate(['/cupon/update', id], {
+      relativeTo: this.route,
+    });
+  }
+
+  crearCupon() {
+    this.router.navigate(['/cupon/create'], {
+      relativeTo: this.route,
+    });
+  }
+  ngOnDestroy(){
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
+}
--- client/src/app/cupon/cupon-routing.module.ts
+++ client/src/app/cupon/cupon-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CuponIndexComponent } from './cupon-index/cupon-index.component';
+import { CuponFormComponent } from './cupon-form/cupon-form.component';
+import { CuponAllComponent } from './cupon-all/cupon-all.component';
 
 const routes: Routes = [
   {
     path: 'cupon', component: CuponIndexComponent
   },
+  {
+    path: 'cupon/all', component: CuponAllComponent
+  },
+  {
+    path:'cupon/create', component: CuponFormComponent
+  }
 ];
 
 @NgModule({
